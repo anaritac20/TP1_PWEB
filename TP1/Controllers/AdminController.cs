ed0b33d7a4b3fd0ab438a93bfe44e08655addc54
BenVictory <38571894+BenVictory@users.noreply.github.com>
2018-12-11 12:04:28 +0000

    Admin listComissaoCurso

---
+++ TP1/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP1.Models;
+
+namespace TP1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private TP1Context context;
+
+        public AdminController()
+        {
+            context = new TP1Context();
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult listComissaoCurso() {
+
+            return View(context.Docentes);
+        }
+
+
+    }
+}
